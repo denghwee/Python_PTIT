81f84c8f01968e782a0754ea83642e5e2df0a5de
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-08-19 23:32:53 +0700

    Update PY01019 - KHOẢNG CÁCH KÝ TỰ.py

--- PY01019 - KHOẢNG CÁCH KÝ TỰ.py
+++ PY01019 - KHOẢNG CÁCH KÝ TỰ.py
@@ -1,7 +1,5 @@
 # I'm DengHwee or you can call me Chimmey
 
-P = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.'
-
 def check(s1, s2):
     for i in range(1, len(s1)):
         if abs(ord(s1[i]) - ord(s1[i - 1])) != abs(ord(s2[i]) - ord(s2[i - 1])):
