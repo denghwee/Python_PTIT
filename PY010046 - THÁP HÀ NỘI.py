b8c5181cf058c1c9eacce8dda4d1ccf7f8563441
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-08-21 00:21:25 +0700

    Create PY010046 - THÁP HÀ NỘI.py

---
+++ PY010046 - THÁP HÀ NỘI.py
@@ -0,0 +1,11 @@
+# I'm DengHwee or you can call me Chimmey
+
+def Try(n, a, b, c):
+    if n == 1:
+        print(a + " -> " + c)
+        return
+    Try(n - 1, a, c, b)
+    Try(1, a, b, c)
+    Try(n - 1, b, a, c)
+
+Try(int(input()), "A", "B", "C")
