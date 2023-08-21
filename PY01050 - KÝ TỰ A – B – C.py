27f09288ec3ebe1a0db33e875605bc53b0b46ef9
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-08-21 23:25:51 +0700

    Create PY01050 - KÝ TỰ A – B – C.py

---
+++ PY01050 - KÝ TỰ A – B – C.py
@@ -0,0 +1,15 @@
+# I'm DengHwee or you can call me Chimmey
+
+def Try(s, l, a, b, c):
+    if len(s) == l:
+        if a <= b and b <= c and a > 0:
+            print(s)
+        return
+    if len(s) < l:
+        Try(s + "A", l, a + 1, b, c)
+        Try(s + "B", l, a, b + 1, c)
+        Try(s + "C", l, a, b, c + 1)
+
+n = int(input())
+for i in range(3, n + 1):
+    Try("", i, 0, 0, 0)
