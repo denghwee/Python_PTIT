53b9e5524d495e3b4cf8a3ad768ba71469141938
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-08-23 07:17:17 +0700

    Create PY01059 - TỔNG CHỮ SỐ - TÍCH CHỮ SỐ.py

---
+++ PY01059 - TỔNG CHỮ SỐ - TÍCH CHỮ SỐ.py
@@ -0,0 +1,18 @@
+# I'm DengHwee or you can call me Chimmey
+
+for i in range(int(input())):
+    s = input()
+    sum = 0
+    product = 1
+    check = 0
+    for i in range(len(s)):
+        if i & 1 == 1:
+            if int(s[i]) != 0:
+                check = 1
+                product *= int(s[i])
+        else:
+            sum += int(s[i])
+    if check == 0:
+        product = 0
+    print(sum, end = ' ')
+    print(product)
