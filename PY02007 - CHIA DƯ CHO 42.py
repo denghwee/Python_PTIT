a4acf6f78bfa6258647ad89425665b21af97dc99
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-08-24 08:22:53 +0700

    Create PY02007 - CHIA DƯ CHO 42.py

---
+++ PY02007 - CHIA DƯ CHO 42.py
@@ -0,0 +1,11 @@
+# I'm DengHwee or you can call me Chimmey
+
+a = []
+
+while len(a) < 10:
+    a += list(map(int, input().split())) 
+
+for i in range(len(a)):
+    a[i] %= 42
+
+print(len(set(a)))
