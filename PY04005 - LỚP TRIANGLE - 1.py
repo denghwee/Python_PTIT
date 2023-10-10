0ed26d71070af9d341815b62655688a5927745d1
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-10-10 17:03:22 +0700

    Create PY04005 - LỚP TRIANGLE - 1.py

---
+++ PY04005 - LỚP TRIANGLE - 1.py
@@ -0,0 +1,39 @@
+# I'm DengHwee or you can call me Chimmey
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def distance(self, a):
+        x0 = self.x - a.x
+        y0 = self.y - a.y
+        return math.sqrt(x0 * x0 + y0 * y0)
+    
+class Triangle:
+    def __init__(self, p1, p2, p3):
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+
+    def calc(self):
+        a = self.p1.distance(self.p2)
+        b = self.p2.distance(self.p3)
+        c = self.p3.distance(self.p1)
+        if (max(a, b, c) * 2 >= a + b + c):
+            print('INVALID')
+        else:
+            print('{:.3f}'.format(a + b + c))
+
+
+
+a = []
+t = int(input())
+for x in range(t):
+    a += [float(i) for i in input().split()]
+i = 0
+for index in range(t):
+    triagle = Triangle(Point(a[i], a[i+1]), Point(a[i+2], a[i+3]), Point(a[i+4], a[i+5]))
+    triagle.cnt()
+    i += 6
