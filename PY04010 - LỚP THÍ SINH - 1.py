0f76867fa545626859ee7e2b36e4993c48a4ca97
Nguyễn Đăng Huy <111379758+denghwee@users.noreply.github.com>
2023-10-16 22:16:19 +0700

    Create PY04010 - LỚP THÍ SINH - 1.py

---
+++ PY04010 - LỚP THÍ SINH - 1.py
@@ -0,0 +1,7 @@
+# I'm DengHwee or you can call me Chimmey
+name = input()
+birth_date = input()
+point_1 = float(input())
+point_2 = float(input())
+point_3 = float(input())
+print(f'{name} {birth_date} {point_1 + point_2 + point_3}')
